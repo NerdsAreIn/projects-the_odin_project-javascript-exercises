7ef79631da5fe60adf192b6b8a94af9de85cae3e
NerdsAreIn <annacantthinkofagoodaddress@hotmail.co.uk>
2021-03-24 01:18:01 +0000

    Test passed

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(testString, repCount) {
+let result = "";
+	if (repCount < 0) result = "ERROR";
+        else if (testString == "") return result;
+	for (let i = 1; i <=repCount; ++i) {
+		result += testString;
+	}
+return result;
 }
-
 module.exports = repeatString
