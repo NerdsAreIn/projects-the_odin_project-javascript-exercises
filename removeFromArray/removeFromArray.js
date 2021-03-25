200060095f2956fb127ffeb7645d2d02465165d7
NerdsAreIn <59518801+NerdsAreIn@users.noreply.github.com>
2021-03-25 11:50:05 +0000

    Update removeFromArray.js

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,7 +1,7 @@
 const removeFromArray = function(testArray, ...toBeRemoved) {  
   for (let n = 0; n < testArray.length; ++n) {
     for (let i = 0; i < toBeRemoved.length; ++i) {
-      if (toBeRemoved[i] === testArray[i]) {
+      if (toBeRemoved[i] === testArray[n]) {
         testArray.splice(n, 1);
       } else continue;
     }
