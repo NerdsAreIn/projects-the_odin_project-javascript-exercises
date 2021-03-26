19bbbe0b4096283c26428d63231954b2b201ee20
NerdsAreIn <annacantthinkofagoodaddress@hotmail.co.uk>
2021-03-26 16:13:06 +0000

    Tests passed

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(testYear) {
+if (testYear % 4 != 0) {
+    return false;
 }
-
+else if (testYear % 4 == 0) {
+    if (testYear % 100 == 0 && testYear % 400 !=0) {
+        return false;
+    } else return true;
+}
+}
 module.exports = leapYears
+
