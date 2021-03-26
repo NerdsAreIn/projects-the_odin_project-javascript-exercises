c40fe66cea9556a165b2ea2367dde227c3251640
NerdsAreIn <annacantthinkofagoodaddress@hotmail.co.uk>
2021-03-26 13:22:20 +0000

    Test passed

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+let bottomNumber;
+let topNumber;
 
+const sumAll = function(firstNumber, secondNumber) {
+    if (!(typeof firstNumber === "number") || !(typeof secondNumber === "number") || (firstNumber < 0 || secondNumber < 0)) {
+return "ERROR";
+   }
+    else if (firstNumber < secondNumber) {
+bottomNumber = firstNumber;
+topNumber = secondNumber;
+   } 
+    else {
+bottomNumber = secondNumber;
+topNumber = firstNumber;
+   }  
+    let result = bottomNumber;
+    for (let i = bottomNumber; i < topNumber; ++i) {
+        bottomNumber = bottomNumber + 1;
+        result += bottomNumber;
+   }
+    return result;
 }
 
 module.exports = sumAll
+
