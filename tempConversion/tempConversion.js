6ce4f3681a737f1adc6713211d3b8b4ab9b38165
NerdsAreIn <annacantthinkofagoodaddress@hotmail.co.uk>
2021-03-28 17:13:31 +0100

    Tests passed

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheitTemp) {
+    let celsiusTemp = (+fahrenheitTemp - 32) * 5/9;
+    if (celsiusTemp != 0) {
+        celsiusTemp = Number(celsiusTemp.toFixed(1));
+    }
+    return celsiusTemp;
 }
 
-const ctof = function() {
-
+const ctof = function(celsiusTemp) {
+    let fahrenheitTemp = +celsiusTemp * 9/5 + 32;
+    if (fahrenheitTemp.toString().match(/\./)) {
+        fahrenheitTemp = Number(fahrenheitTemp.toFixed(1));
+    } 
+    return fahrenheitTemp;
 }
 
 module.exports = {
