6fed33e9ea5bcc7cd1ffdfa575239b1d1ffa7d39
NerdsAreIn <annacantthinkofagoodaddress@hotmail.co.uk>
2021-03-30 00:37:23 +0100

    All done

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,7 +1,9 @@
 const leapYears = function(testYear) {
+// branch one - flow stops for years that are definitely not leap years
 if (testYear % 4 != 0) {
     return false;
 }
+// branch two - for possible leap years, which face a further subtest
 else if (testYear % 4 == 0) {
     if (testYear % 100 == 0 && testYear % 400 !=0) {
         return false;
