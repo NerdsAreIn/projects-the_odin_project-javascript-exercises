8d7020df196eb63078ebd8d3b9ff7aa6f7b0ed37
NerdsAreIn <annacantthinkofagoodaddress@hotmail.co.uk>
2021-05-20 22:18:52 +0100

    tests passed

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -35,8 +35,14 @@ function power(a, b) {
 return a**b;	
 }
 
+// the factorial of a number is the product of all of the positive numbers up to and including that number. E.g., the factorial of 5 is 120 - i.e.,
+5 x 4 x 3 x 2 x 1 = 120 
 function factorial(number) {
 	let result = number;
+//  condition to deal with the strange case of 0, whose factorial is, counterintuitively, 1 
+	if (number == 0) {
+		result = 1;
+	}
 	while (number > 1) {
 		result *= number - 1;
 		number--;
