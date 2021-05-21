496dca0f5a21421aebc014f9883e11ec654ca2ec
NerdsAreIn <59518801+NerdsAreIn@users.noreply.github.com>
2021-05-22 00:44:45 +0100

    Update palindromes.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    const newString = string.match(/[a-z]/gi).reverse().join("");
+    string = string.match(/[a-z]/gi).join("");
+    if (newString.toLowerCase() == string.toLowerCase()) {
+        return true;
+    }
+    else return false;
 }
 
 module.exports = palindromes
