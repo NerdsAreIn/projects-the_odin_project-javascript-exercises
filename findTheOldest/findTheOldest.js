aa76b4622e4a827488f02cab2b5786889529a436
NerdsAreIn <59518801+NerdsAreIn@users.noreply.github.com>
2021-05-24 16:06:56 +0100

    Update findTheOldest.js

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(people) {
+    let oldest;
+    let age1;
+    let age2;
+    const peopleSorted = people.sort((personA, personB) => {
+        age1 = personA.yearOfDeath - personA.yearOfBirth;
+        age2 = personB.yearOfDeath - personB.yearOfBirth;
+        if (personA.yearOfDeath == null) {
+            let currentYear = new Date().getFullYear();
+            age1 = currentYear - personA.yearOfBirth;
+        }
+        if (age1 < age2) {
+            return 1;
+        }   
+     });
+    oldest = peopleSorted[0];
+    return oldest; 
 }
+
 
 module.exports = findTheOldest
