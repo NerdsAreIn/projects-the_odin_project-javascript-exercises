1bf3fa7f1f8440c26309d4c2a5a7b730250b235a
NerdsAreIn <59518801+NerdsAreIn@users.noreply.github.com>
2021-05-24 14:16:40 +0100

    Update fibonacci.js

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,11 +1,13 @@
 const fibonacci = function(number) {
     let fibonacciSequence = [0, 1];
+    if (number < 0) {
+        return "OOPS";
+        }
     for (let i = 2; i <= number; ++i) {
         fibonacciSequence[i] = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
      } 
     console.log(fibonacciSequence);
     return fibonacciSequence[number];        
 }
-
 
 module.exports = fibonacci
